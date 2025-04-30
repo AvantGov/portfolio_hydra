ad1d182e277d0e7f8f82656b2883f56249afcad1
Alex <44531619+AvantGov@users.noreply.github.com>
2025-04-30 08:54:32 -0500

    Update Footer.jsx
    updating the spotify link in the footer to go to the on repeat playlist

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
                 <a href="https://www.behance.net/AlexCouts" target="_blank" rel="noopener noreferer" className="socialContainer__svgLink" id='behance'>
                     <img className="svglink__img" id='behance_svg' src='Behance_logo.svg' />
                 </a>
-                <a href="https://open.spotify.com/artist/11HMbZOFDN6Qd6pjZTtrzY?si=n4QAF5rsRpm8pp5vjswF-w" target="_blank" rel="noopener noreferer" className="socialContainer__svgLink">
+                <a href="https://open.spotify.com/playlist/6a6WHsODpZRCD4lUwyYVdN?si=1bbcd7e565a24486" target="_blank" rel="noopener noreferer" className="socialContainer__svgLink">
                     <img className="svglink__img" id='spotify_svg' src='spotify-svgrepo-com.svg' />
                 </a>
                 <a href="https://www.linkedin.com/in/alex-c-329721109/" target="_blank" rel="noopener noreferer" className="socialContainer__svgLink">
