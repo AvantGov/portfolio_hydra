95a79d2734f29f63429997f8b2ab9d132f0ba29b
Alex <44531619+AvantGov@users.noreply.github.com>
2025-07-14 11:24:27 -0500

    Update Heading.jsx
    fixing small punctuation issue, commit direct to main.

--- client/src/Components/Home/Heading.jsx
+++ client/src/Components/Home/Heading.jsx
@@ -8,7 +8,7 @@ const Heading = () => {
             <h1 className='Heading__title'>Alex C.</h1>
             <h2 className='Heading__subtitle'>Web Developer</h2>
             <br />
-            <p className='Heading__leade'>With a background spanning web development, ERP/CRM development, and quality assurance, I have designed digital solutions for employers and clients by translating complex business processes into intuitive applications and workflows that delight end users and empower stakeholders to run their businesses effectively and efficiently. I am a versatile developer with experience building Quickbase and Salesforce applications, integrating automation systems, and delivering striking websites with modern javascript frameworks.</p>
+            <p className='Heading__leade'>With a background spanning web development, ERP/CRM development, and quality assurance; I have designed digital solutions for employers and clients by translating complex business processes into intuitive applications and workflows that delight end users and empower stakeholders to run their businesses effectively and efficiently. I am a versatile developer with experience building Quickbase and Salesforce applications, integrating automation systems, and delivering striking websites with modern javascript frameworks.</p>
         </div>
     )
 }
